7fd6be1671f31d6992b8bfe6537c65a4a0f23d29
Matyáš Strelec <matyasstrelec@gmail.com>
2023-10-24 16:38:41 +0200

    simple landing page

---
+++ caloricalc/src/lib/components/Navbar.svelte
@@ -0,0 +1,10 @@
+<div class="navbar bg-base-100">
+	<div class="flex-1">
+		<a class="btn btn-ghost normal-case text-xl" href="/">caloriCalc</a>
+	</div>
+	<div class="flex-none">
+		<ul class="menu menu-horizontal px-1">
+			<li><a href="/about">About</a></li>
+		</ul>
+	</div>
+</div>
--- caloricalc/src/routes/+page.svelte
+++ caloricalc/src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>caloriCalc</h1>
-<p>A better calories tracker</p>
+<div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">caloriCalc</h1>
+        <p class="py-6">The best companion app to help you reach your nutrition goals</p>
+        <button class="btn btn-primary">Get Started</button>
+      </div>
+    </div>
+  </div>
