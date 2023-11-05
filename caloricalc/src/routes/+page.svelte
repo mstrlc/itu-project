152b2e3975874240d9403bafdb28ae364497aeb4
Matyáš Strelec <matyasstrelec@gmail.com>
2023-11-05 18:34:07 +0100

    simplify landing page

--- caloricalc/src/routes/+page.svelte
+++ caloricalc/src/routes/+page.svelte
@@ -1,23 +1,8 @@
-<script>
-	var response = '';
-
-	async function testAPI() {
-		const res = await fetch('/api/test', {
-			method: 'GET'
-		});
-		const data = await res.json();
-		response = data.message + ' at ' + data.time;
-	}
-</script>
-
 <div class="hero min-h-screen bg-base-200">
 	<div class="hero-content text-center">
 		<div class="max-w-md">
 			<h1 class="text-5xl font-bold">caloriCalc</h1>
 			<p class="py-6">The best companion app to help you reach your nutrition goals</p>
-			<button class="btn btn-primary" on:click={testAPI}>Test API</button>
-			<p>Response from API:</p>
-			<p>{response}</p>
 		</div>
 	</div>
 </div>
