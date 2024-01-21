a6b4c248e1af9d22f80c2dcff9389618b7ab8100
Matyáš Strelec <matyasstrelec@gmail.com>
2024-01-21 18:37:13 +0100

    layout fix

--- caloricalc/src/routes/+layout.svelte
+++ caloricalc/src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script>
 	import '../app.css';
 	import Navbar from '../lib/components/Navbar.svelte';
-	
-
 </script>
 
 <Navbar />
 
 <div class="h-screen">
-	<slot />
+	<div class="container mx-auto px-32">
+		<div class="h-12" />
+		<slot />
+	</div>
 </div>
-
